775680562c6d697c39dc495f4e5b6493a782172d
@hexlet/cli <support@hexlet.io>
2024-12-18 23:54:37 +0500

    submit java-spring-ru/dependency-injection

---
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -0,0 +1,62 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import exercise.exception.ResourceNotFoundException;
+
+// BEGIN
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
+    final CommentRepository commentRepository;
+
+    public CommentsController(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @GetMapping
+    public List<Comment> index() {
+        return commentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Comment getComment(@PathVariable Long id) {
+        return commentRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment addComment(@RequestBody Comment comment) {
+        commentRepository.save(comment);
+        return comment;
+    }
+
+    @PutMapping("/{id}")
+    public Comment updateComment(@PathVariable Long id, @RequestBody Comment comment) {
+        var commentToUpdate = commentRepository.findById(id);
+        if (commentToUpdate.isPresent()) {
+            var updatedComment = commentToUpdate.get();
+            updatedComment.setBody(comment.getBody());
+            updatedComment.setPostId(comment.getPostId());
+            commentRepository.save(updatedComment);
+            return updatedComment;
+        } else {
+            throw new ResourceNotFoundException("This comment does not exists");
+        }
+    }
+}
+// END
---
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,70 @@
+package exercise.controller;
+
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+
+// BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+    final PostRepository postRepository;
+    final CommentRepository commentRepository;
+
+    public PostsController(PostRepository postRepository, CommentRepository commentRepository) {
+        this.postRepository = postRepository;
+        this.commentRepository = commentRepository;
+    }
+
+    @GetMapping
+    public List<Post> index() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPost(@PathVariable Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post addPost(@RequestBody Post post) {
+        postRepository.save(post);
+        return post;
+    }
+
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+        var mayPost = postRepository.findById(id);
+        if (mayPost.isPresent()) {
+            var toUpdatePost = mayPost.get();
+            toUpdatePost.setTitle(post.getTitle());
+            toUpdatePost.setBody(post.getBody());
+            postRepository.save(toUpdatePost);
+            return toUpdatePost;
+        } else {
+            throw new ResourceNotFoundException("This post does not exists");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        commentRepository.deleteByPostId(id);
+        postRepository.deleteById(id);
+    }
+}
+// END
